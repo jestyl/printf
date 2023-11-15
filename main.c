2c42d171f52a3ac4bba4c321030462e35da5698d
Jestyl <jestylarku@gmail.com>
2023-11-15 01:27:24 -0800

    reversed

--- main.c
+++ main.c
@@ -39,9 +39,7 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
-	
-	_printf("%S\n", "Best\nSchool");
+	printf("Unknown:[%r]\n");;
 
 	return (0);
 }
